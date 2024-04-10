eae5342d80a0f3689553870eafca63109a1b217a
khushbul10 <khushbulalam10@gmail.com>
2024-04-10 14:03:58 +0600

    up2:03

--- myscript.js
+++ myscript.js
@@ -3,3 +3,5 @@ $(window).on("load resize ", function() {
     var scrollWidth = $('.tbl-content').width() - $('.tbl-content table').width();
     $('.tbl-header').css({'padding-right':scrollWidth});
   }).resize();
+
+  
